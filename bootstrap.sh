d65e021dc7942db53ed2f946fc3ddfdff5153993
Bernt Andreas Langøien <berntan@usit.uio.no>
2013-03-26 20:55:53 +0100

    scalariform executable

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync -av bin ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
